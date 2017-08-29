7c854b6af87ebe4957905ed1cf12a30f2c575fe5
roxanavp87 <roxanavp87@gmail.com>
2017-08-29 12:26:02 -0500

    create bots

--- main.js
+++ main.js
@@ -5,8 +5,12 @@
 (function () {
     "use strict";
 
-
-    var names = "";
+    function Bot(name, speed, strength, agility) {
+        this.name = name;
+        this.speed = speed;
+        this.strength = strength;
+        this.agility = agility;
+    }
 
     function random(min, max) {
         return Math.round(Math.random()*(max-min)) + min;
@@ -48,19 +52,21 @@
          * numberOfNames = number of names to generate
          */
         var seq = new Set();
+        var names = "";
         while(seq.size < numberOfNames) {
             seq.add(generateName());
         }
 
-        seq.forEach(function (element) {
-            console.log(element);
-            names += "<p>" + element + "</p>";
-        });
+        // seq.forEach(function (element) {
+        //     console.log(element);
+        //     names += "<p>" + element + "</p>";
+        // });
+        return seq;
     }
 
-    generateUniqueNames(15);
 
-    $('#bots-names').html(names);
+
+    // $('#bots-names').html(names);
 
     function generateNumbers() {
         /**
@@ -89,12 +95,43 @@
         return numbers;
     }
 
-    var numbers = generateNumbers();
-    numbers.forEach(function (element) {
-        console.log(element);
-    });
+    // var numbers = generateNumbers();
+    // numbers.forEach(function (element) {
+    //     console.log(element);
+    // });
+    //
+    // console.log("total sum" + sum(numbers));
 
-    console.log("total sum" + sum(numbers))
+    function createBots() {
+        var bots = [];
+        var names = generateUniqueNames(15);
+        var it = names.values();
+        var numbers = generateNumbers();
+        var attr = [];  // attr[0]=speed, attr[1]=strength, attr[2]=agility;
+        for(var i=0; i<numbers.length; i++) {
+            for(var j=0; j<3; j++) {
+                attr[j] = Math.floor(numbers[i]/3); //quotient
+            }
+            attr[random(0,2)] += numbers[i] % 3;  //remainder
+            bots[i] = new Bot(it.next(), attr[0], attr[1], attr[2]);
+
+            // var Bot = {
+            //     name: names[i],
+            //     speed: attr[0],
+            //     strength: attr[1],
+            //     agility: attr[2]
+            // };
+            //
+            // bots[i] = Object.create(Bot);
+            // console.log(numbers[i] + ", " + attr[0]+ ", " + attr[1]+ ", " + attr[2])
+        }
+        return bots;
+    }
+
+    var bots = createBots();
+    console.log(bots);
+
+
 
 
 
